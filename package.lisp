a62f566d9e500b2d1f333fee66c4208c1b97263f
Nick Matvyeyev <mnasoft@gmail.com>
2021-06-11 09:31:32 +0300

    11 июн 2021 г.  9:31:32 изменен метод print-object класса <gas>

--- package.lisp
+++ package.lisp
@@ -24,6 +24,7 @@
 	   <element>
 	   <truba>
 	   <zabornik>
+           <troynik>
 	   <metallorukav>
 	   <perehod>
 	   <forsunka>
--- src/methods.lisp
+++ src/methods.lisp
@@ -2,45 +2,39 @@
 
 (in-package #:idelchik)
 
-(defmethod μ ((g <gas>))
-  (values (varghaftik:μ :gas_name (name g))"[kg/mol]" "Молекулярная масса газа" (format nil "~S" g)))
+(defmethod μ ((gas <gas>))
+  (values (varghaftik:μ :gas_name (name gas)) "[kg/mol]" "Молекулярная масса газа"))
 
-(defmethod k ((g <gas>))
-  (values (varghaftik:k :gas_name (name g))"[1]" "Показатель адиабаты" (format nil "~S" g)))
+(defmethod k ((gas <gas>))
+  (values (varghaftik:k :gas_name (name gas))"[1]" "Показатель адиабаты" ))
 
-(defmethod ρ ((p <param>) (g <gas>))
-  (values (/ (* (pressure p) (μ g) ) (* varghaftik:Rμ (tempreche p)))
-	  "[kg/m^3]" "Плотность газа"
-	  (format nil "~S" g)
-	  (format nil "~S" p)))
+(defmethod ρ ((param <param>) (gas <gas>))
+  (values (/ (* (pressure param) (μ gas) ) (* varghaftik:Rμ (tempreche param)))
+	  "[kg/m^3]" "Плотность газа"))
 
-(defmethod η((p <param>) (g <gas>))
-  (values (varghaftik:η_sazerlend (tempreche p) :<gas>_name (name g))
+(defmethod η((p <param>) (gas <gas>))
+  (values (varghaftik:η_sazerlend (tempreche p) :gas_name (name gas))
 	  "[Pa*s]"
-	  "Коэффициент динамической вязкости газа"
-	  (format nil "~S" g)
-	  (format nil "~S" p)))
+	  "Коэффициент динамической вязкости газа"))
 
-(defmethod ν ((p <param>) (g <gas>))
-  (values
-   (/ (η p g) (ρ p g))
-   "[m^2/s]"
-   (format nil "Коэффициент кинематической вязкости газа ~S ~S" (format nil "~S" g) (format nil "~S" p))))
+(defmethod ν ((param <param>) (gas <gas>))
+  (values (/ (η param gas) (ρ param gas))
+          "[m^2/s]" "Коэффициент кинематической вязкости газа"))
 
-(defmethod <rib>-name ((x <rib>))
+(defmethod <rib>-name ((rib <rib>))
     "Выполняет вывод на печать списка рёбер в форме пригодной для вставки в исходный код
 Пример исползования:
 (mapcar #'(lambda (el) (<rib>-name el)) ribs)
 "
     (format T "(make-instance '<gidro-rib> :v1 \"~A\" :v2 \"~A\" :name \"~A-~A\")~%"
-	    (<rib>-v1 x)
-	    (<rib>-v2 x)
-	    (<rib>-v1 x)
-	    (<rib>-v2 x)))
+	    (<rib>-v1 rib)
+	    (<rib>-v2 rib)
+	    (<rib>-v1 rib)
+	    (<rib>-v2 rib)))
 
-(defmethod mk-rib ((x <element>))
+(defmethod mk-rib ((element <element>))
   "Создаёт список рёбер, основанный на списке вершин элемента."
-  (let ((v-lst (vertexes x)))
+  (let ((v-lst (vertexes element)))
     (cond
       ((= 2 (length v-lst))
        (list (make-<gidro-rib> (first v-lst) (second v-lst))))
